7fc6b6a4b908c13a568164502fa9077a97827bc1
Daniel Henrique <danielhfc01@gmail.com>
2024-07-15 16:13:00 -0300

    Initial commit

---
+++ src/Components/SideBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SideBar() {
+  return (
+    <div className="w-[90px] h-[90px] bg-black">
+               
+            </div>
+  )
+}
+
+export default SideBar
