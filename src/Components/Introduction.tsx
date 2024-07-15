5c8f5b668f758322f2689d743b6b0cfc9bf23c17
Daniel Henrique <danielhfc01@gmail.com>
2024-07-15 17:10:06 -0300

    [ADD] Seção de introdução

---
+++ src/Components/Introduction.tsx
@@ -0,0 +1,13 @@
+function Introduction() {
+  return (
+    <div className="flex justify-center flex-col items-center">
+        <div className="h-[120px] border-r-[1px] border-black"/>
+        <div className="w-[5px] h-[5px] bg-green-600 rounded-full"></div>
+        <h2 className="mt-5 font-medium text-[13px]">Hello! My name is</h2>
+        <h2 className="text-[70px] font-bold tracking-widest mt-5 ml-20">Daniel <br />Henrique</h2>
+        <img src="/avatar.jpg" alt="Avatar" className="w-[160px] h-[160px] bg-gray-200 p-7 rounded-full mt-7"/>
+    </div>
+  )
+}
+
+export default Introduction
